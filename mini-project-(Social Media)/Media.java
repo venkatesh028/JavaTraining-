d61a088b1166e06ed7b74919673bf27f60d88ac6
Venkatesh.T.M <76031942+venkatesh028@users.noreply.github.com>
2022-09-12 13:54:52 +0530

    Add files via upload

--- mini-project-(Social Media)/Media.java
+++ mini-project-(Social Media)/Media.java
@@ -1,6 +1,11 @@
 import com.ideas2it.view.UserView;
 
-
+/**
+ * call the home page in userView 
+ *
+ * @version 1.0 12-SEP-2022
+ * @author Venkatesh TM
+ */
 public class Media {
     public static void main(String[] args) {
         UserView userView = new UserView();        
